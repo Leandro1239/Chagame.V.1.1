62594d052784e980c3bc6b23e2acf560b85c8b1f
Leandro <leandro.cruz.oliveira@hotmail.com>
2019-03-14 18:10:22 -0300

    Versão da reunião 15/03
    Coletando açai
    Recuperando vida na barraca
    Com árvore e amarelinho
    Cenário movendo mas perdendo qualidade

--- Assets/Códigos/Movimentacao.cs
+++ Assets/Códigos/Movimentacao.cs
@@ -11,13 +11,13 @@ public class Movimentacao : MonoBehaviour {
     float forca = 20;                           //JOGADOR
     float VelocidadeAceleração = 10;
     public Rigidbody2D player;
-    private bool tocachao = false;
+    public bool tocachao = false;
     Rigidbody2D rigd = new Rigidbody2D();           
     
 
-    public Transform checachao;                     //CHÃO
-    public LayerMask piso;
-    float chaograu = 0.2f;
+    //public Transform checachao;                     //CHÃO
+    //public LayerMask piso;
+    //float chaograu = 0.3f;
     
 
     public GameObject Largada;                      //RECORDE
@@ -36,47 +36,45 @@ public class Movimentacao : MonoBehaviour {
     
 	}
 
-    private void Update()                                                       // MEDE A DISTANCIA E COLOCA EM 'RECORDE'
+    private void Update()                                                       
     {
-        distancia = Vector2.Distance(Largada.transform.position, player.transform.position);
+        distancia = Vector2.Distance(Largada.transform.position, player.transform.position);  // MEDE A DISTANCIA E COLOCA EM 'RECORDE'
         recorde = (int)distancia;
         Total_Distancia.text = recorde.ToString();
 
         rigd.velocity = new Vector2(VelocidadeAceleração, rigd.velocity.y);               //FAZ CORRER SOZINHO
-        tocachao = Physics2D.OverlapCircle(checachao.position, chaograu, piso);             //CONTATO COM O CHAO
+        //tocachao = Physics2D.OverlapCircle(checachao.position, chaograu, piso);             //CONTATO COM O CHAO
 
-       /* if (tocachao == false)
-        {
-            anim.SetBool("Pular", false);
-            anim.SetBool("Correr", true);
-        }*/
     }
 
     public void Pula()
     {
-        if (tocachao)                            //PULA com o botão
+        if (tocachao == true)                            //PULA com o botão
         {
             player.AddForce(new Vector2(0, forca));                                         //SAI DO CHÃO
             anim.SetBool("Pular", true);
             anim.SetBool("Correr", false);
             VelocidadeAceleração = VelocidadeAceleração * 1.02f;                            //ATRIBUI O AUMENTO DA VELOCIDADE A CADA PULO
+            tocachao = false;                                                               //CONTATO COM O CHAO
         }
     }
 
     public void OnCollisionEnter2D(Collision2D Chao)
     {
         if (Chao.gameObject.CompareTag("Chão"))
-        { 
+        {
+            tocachao = true;
             anim.SetBool("Pular", false);
             anim.SetBool("Correr", true);
         }
+
     }
 
-    public void OnTriggerEnter2D(Collider2D ativar)                         //COLETA E CONTA MOEDA
+    public void OnTriggerEnter2D(Collider2D coletar)                         //COLETA E CONTA MOEDA
     {
-        if (ativar.gameObject.CompareTag("coin"))
+        if (coletar.gameObject.CompareTag("coin"))
         {
-            Destroy(ativar.gameObject);
+            Destroy(coletar.gameObject);
             moedas += 1;
             Total_Moeda.text = moedas.ToString();
         }
---
+++ Assets/Códigos/MovimentoDeCenário.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoDeCenário : MonoBehaviour
+{
+    float velChao = 0.4f;
+    float velPredio = 0.3f;
+    public Renderer Chao;
+    public Renderer Predio;
+       
+    void Update()
+    {
+        Vector2 offset = new Vector2(velChao * Time.deltaTime, 0);
+        Chao.material.mainTextureOffset += offset;
+
+        Vector2 offset2 = new Vector2(velPredio * Time.deltaTime, 0);
+        Predio.material.mainTextureOffset += offset2;
+    }
+}
